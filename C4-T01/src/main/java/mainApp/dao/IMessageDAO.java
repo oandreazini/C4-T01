00a47f2c3f417cdff3da297964fa1f982831b28b
oandreazini <55554433+oandreazini@users.noreply.github.com>
2022-08-23 12:16:16 +0200

    Delete C4-T01 directory

--- C4-T01/src/main/java/mainApp/dao/IMessageDAO.java
+++
@@ -1,9 +0,0 @@
-package mainApp.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import mainApp.dto.Message;
-
-public interface IMessageDAO extends JpaRepository<Message, Integer>{ 
-
-}
